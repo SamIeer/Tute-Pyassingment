4cc0251dd77ba42c6e51e49a347a6d748ba8c696
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-04-03 01:41:12 +0530

    assingment 1

---
+++ Assingment1.py
@@ -0,0 +1,14 @@
+#question 1
+# num = int(input("enter first numbwr:"))
+# num1 = int(input("enter first numbwr:"))
+
+# print("addition",num+num1)
+# print("subtraction",num-num1)
+# print("multipication",num*num1)
+# print("division",num/num1)
+
+
+#question 2
+name = input("Enter your Firstname: ")
+lname = input("Enter your Lirstname: ")
+print(f"Hello,{name} {lname}! welcome to my python program.")
